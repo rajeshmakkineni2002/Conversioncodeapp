6a0561f823c96d46382f17986fabaf7aac00824a
Rajesh-makkineni <makkinenirajesh@gmail.com>
2025-08-19 18:34:35 +0100

    Conversioncodeapp

---
+++ Conversioncodeapp.java
@@ -0,0 +1,28 @@
+import java.text.SimpleDateFormat;
+import java.util.Scanner;
+public class Conversioncodeapp 
+{
+    public static void main(String [] args) throws Exception
+    {
+        //read the input from the user
+        Scanner scanner=new Scanner (System.in);
+        System.out.println("Enter the  date : : (dd-MM-yyyy)");
+        String sdate = scanner.next();
+
+        //convert the date from string formant to java util.date
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+        java.util.Date uDate = sdf.parse(sdate);
+
+        //convert java.util. date to java.sql.date
+        long value = uDate.getTime();
+         java.sql.Date sqlDate = new java.sql.Date(value);
+
+         //printing all 3 formats of date
+         System.out.println(" String format date is : : " + sdate);
+         System.out.println( "util date is : : " + uDate);
+         System.out.println(" Sql date is : : " + sqlDate);
+
+
+         scanner.close();
+    }    
+}
